1bf84578da182b1e42fb47b0b184683c7c1990c5
jilanishaik <ashaik.jilani@gmail.com>
2021-12-30 23:33:46 +0400

    intial version with Frontend React files

--- src/main/java/com/learning/jilani/controller/TeamController.java
+++ src/main/java/com/learning/jilani/controller/TeamController.java
@@ -7,10 +7,12 @@ import com.learning.jilani.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class TeamController {
 
